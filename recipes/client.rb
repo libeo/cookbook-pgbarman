eeceaff90201edd521cae081483af98257073a25
Alfredo Jose Fernandes Palhares <masterkorp@masterkorp.net>
2014-01-23 15:48:45 +0000

    Deploy ssh keys to the barman user.

--- recipes/client.rb
+++ recipes/client.rb
@@ -9,3 +9,25 @@ user node["pgbarman"]["user"] do
   home "/home/#{node["pgbarman"]["user"]}"
   shell "/bin/bash"
 end
+
+ssh_keys = data_bag_item("pgbarman", "barman")
+
+directory "/home/#{node["pgbarman"]["user"]}/.ssh" do
+  owner node["pgbarman"]["user"]
+  group node["pgbarman"]["user"]
+  mode 00700
+end
+
+file "/home/#{node["pgbarman"]["user"]}/.ssh/id_rsa" do
+  owner node["pgbarman"]["user"]
+  group node["pgbarman"]["user"]
+  mode 00600
+  content ssh_keys["private_key"]
+end
+
+file "/home/#{node["pgbarman"]["user"]}/.ssh/authozized_keys" do
+  owner node["pgbarman"]["user"]
+  group node["pgbarman"]["user"]
+  mode 00600
+  content ssh_keys["private_key"]
+end
