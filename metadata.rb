0c9fcc90204b8fe500a79c111952a0b9fcd3a891
Maxime Pelletier <pelletiermaxime@gmail.com>
2015-02-26 18:24:56 -0500

    Bump the version and change the maintainer to Libeo. There's not much left from the original cookbook hehe

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'pgbarman'
-maintainer       'Geoforce, Inc'
-maintainer_email 'masterkorp@masterkorp.net'
+maintainer       'Libeo'
+maintainer_email 'maxime.pelletier@libeo.com'
 license          'All rights reserved'
 description      'Installs/Configures pgbarman'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 %w(postgresql python rsync build-essential tar user).each do |ckbk|
   depends ckbk
